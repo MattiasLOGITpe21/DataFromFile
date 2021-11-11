bd07b9f08c00d4ccf0dab66d7c2fa3e76ea0028e
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:25:05 +0200

    data from file

---
+++ DataFromFile/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your favourite color?:");
+            string favColour = Console.ReadLine();
+
+            string output = GetLineFromfileData(favColour);
+
+            if(output.Length != 0)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, color couldn't be found");
+            }
+
+        }
+
+
+
+        private static string[] ReadDataFromFile()
+        {
+            string directoryPath = @"C:\users\opilane\samples";
+            string fileName = "personality.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+
+            return dataFromFile;
+        }
+
+        private static string GetLineFromfileData(string userInput)
+        {
+            string[] dataFromFile = ReadDataFromFile();
+            string result = "";
+
+
+
+
+            foreach (string line in dataFromFile)
+            {
+                if (line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+                }
+
+            }
+            return result;
+        }
+    }
+}
---
+++ Horoscope/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your star sign:");
+            string starSign = Console.ReadLine();
+
+            string output = GetLineFromfileData(starSign);
+
+            if (output.Length != 0)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, star sign couldn't be found");
+            }
+
+        }
+
+
+
+        private static string[] ReadDataFromFile()
+        {
+            string directoryPath = @"C:\users\opilane\samples";
+            string fileName = "horoscope.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+
+            return dataFromFile;
+        }
+
+        private static string GetLineFromfileData(string userInput)
+        {
+            string[] dataFromFile = ReadDataFromFile();
+            string result = "";
+
+
+
+
+            foreach (string line in dataFromFile)
+            {
+                if (line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+                }
+
+            }
+            return result;
+        }
+    }
+}
